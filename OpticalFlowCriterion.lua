98e6c9607aa5c14820ac060272c7e078d701c990
Alena Moravova <moraval2@lcgpu.felk.cvut.cz>
2016-08-15 14:40:19 +0200

    changed optical flow

--- OpticalFlowCriterion.lua
+++ OpticalFlowCriterion.lua
@@ -1,7 +1,13 @@
+local THNN = require 'nn.THNN'
 local OpFlowCriterion, parent = torch.class('nn.OpticalFlowCriterion', 'nn.Criterion')
 require 'image'
-batchSize = 6
---torch.setdefaulttensortype('torch.FloatTensor')
+batchSize = 24
+epoch = 5
+
+alfa = 0.2
+
+norm1 = 1
+norm2 = 1
 
 function OpFlowCriterion:__init()
   parent.__init(self)
@@ -13,18 +19,56 @@ end
 function OpFlowCriterion:updateOutput (flows, images)
   self.output = 0
 
-  for i=1,batchSize do
---    warp the optical flow into image
-    image_estimate = image.warp(images[i][1], flows[i], 'simple', false)
+  size1 = images[1][1]:size(1)
+  size2 = images[1][1]:size(2)
 
-    self.output = self.output + torch.sum(torch.abs(image_estimate - images[i][2]))
-    image_estimate = nil
+  image_estimate = torch.Tensor(batchSize, 1, size1, size2)
+  local targets = torch.Tensor(batchSize, 1, size1, size2)
+
+  for i=1,batchSize do
+----    warp the optical flow into image
+----    image_estimate = image.warp(images[i][1], flows[i])
+    image_estimate[i] = image.warp(images[i][1], flows[i], 'bilinear', true)
+    targets[i] = images[i][2]
   end
 
---  return self.output, self.total_weight_tensor[1]
+--  self.sizeAverage = true
+--  self.output_tensor = self.output_tensor or input.new(1)
+--  self.output_tensor = self.output_tensor or torch.Tensor(1)
+
+--  input.THNN.MSECriterion_updateOutput(
+--    THNN.OpticalFlowCriterion_updateOutput(
+--    image_estimate:cdata(),
+--    targets:cdata(),
+--    self.output_tensor:cdata(),
+--    size1,
+--    size2,
+--    batchsize,
+--    1,
+--    self.sizeAverage
+--  )
+--  self.output = self.output_tensor[1]
+
+  differences = image_estimate - targets
+  
+  self.output = torch.sum(torch.abs(image_estimate - targets))
   return self.output
 end
 
+--
+
+
+function regularize(flow, r, s)
+  my_sum = torch.Tensor(2)
+
+  local help = flow:sub(1,1,r-1,r+1,s-1,s+1)
+  my_sum[1] = help:sum() - help[1][2][2]
+
+  help = flow:sub(2,2,r-1,r+1,s-1,s+1)
+  my_sum[2] = help:sum() - help[1][2][2]
+
+  return my_sum/8
+end
 --
 --input = optical flow (values for u,v), target = image
 function OpFlowCriterion:updateGradInput (flows, images)
@@ -33,88 +77,81 @@ function OpFlowCriterion:updateGradInput (flows, images)
   self.gradInput:resizeAs(flows):zero()
 
   for i=1,batchSize do
-    start_image = images[i][1]
-    target = images[i][2]
-
-    image_estimate_small = image.warp(start_image, flows[i], 'simple', false)
+    local start_image = images[i][1]
+    local target = images[i][2]
 
     size1 = target:size(1)
     size2 = target:size(2)
 
-    image_estimate = torch.Tensor(size1 + 2, size2 + 2)
-
-    help = image_estimate:sub(2, size1+1, 2, size2+1)
-    help:copy(image_estimate_small)
-
-    image_estimate_small = nil
-
-    diff = torch.FloatTensor(3, size1, size2):fill(0)
-
---copy the needed shifts to diffU or diffV and then subtract it
-    a = diff:sub(2,2, 1,size1, 1,size2)
-    b = diff:sub(1,1, 1,size1, 1,size2)
-    c = diff:sub(3,3, 1,size1, 1,size2)
-
-    help = image_estimate:sub(2, size1+1, 2, size2+1)
-    a = torch.abs(help - target)
-
-    help = image_estimate:sub(1, size1, 2, size2+1)
-    b = torch.abs(help - target)
-
-    help = image_estimate:sub(3, size1+2, 2, size2+1)
-    c = torch.abs(help - target)
-
-    _, min_dir = torch.min(diff, 1)
-
-    help_tensor = torch.DoubleTensor(1,size1,size2)
-    for j=1,size1 do
-      for l=1,size2 do
-        help_tensor[1][j][l] = min_dir[1][j][l]
+    if (epoch % 5 == 0 or epoch == 1) then
+      if (i==1) then
+        my_string = ''
+        for r=1,size1,30 do
+          for s=1,size2,60 do
+            my_string = my_string..flows[i][1][r][s]..' '
+          end
+        end
+        print('before normalization')
+        print(my_string)
       end
     end
-    min_dir = nil
 
-    grad = self.gradInput:sub(i,i, 1,1)
-    grad:add(help_tensor - 2)
-    help_tensor = nil
+    -- NORMALIZE
+    flows[i][1] = flows[i][1] *norm1
+    flows[i][2] = flows[i][2] *norm2
 
-    diff:fill(0)
-
-    a = diff:sub(2,2, 1,size1, 1,size2)
-    b = diff:sub(1,1, 1,size1, 1,size2)
-    c = diff:sub(3,3, 1,size1, 1,size2)
-
-    help = image_estimate:sub(2, size1+1, 2, size2+1)
-    a = torch.abs(help - target)
-
-    help = image_estimate:sub(2, size1+1, 1, size2)
-    b = torch.abs(help - target)
-
-    help = image_estimate:sub(2, size1+1, 3, size2+2)
-    c = torch.abs(help - target)
-
---    grad2 = grad2 + torch.min(diff, 3)[2] - 2
-    _, min_dir = torch.min(diff, 1)
-
-    help_tensor = torch.DoubleTensor(1,size1,size2)
-    for j=1,size1 do
-      for l=1,size2 do
-        help_tensor[1][j][l] = min_dir[1][j][l]
+    if (epoch % 5 == 0 or epoch == 1) then
+      if (i==1) then    
+        my_string = ''
+        my_string_2 = ''
+        for r=1,size1,30 do
+          for s=1,size2,60 do
+            my_string = my_string..flows[i][1][r][s]..' '
+          end
+        end
+        print('after normalization')
+        print(my_string)
       end
     end
-    min_dir = nil
 
-    grad2 = self.gradInput:sub(i,i, 2,2)
-    grad2:add(help_tensor - 2)
-    
-    help_tensor = nil
-    start_image = nil
-    target = nil
-    print('Done: '..i)
+    local image_estimate_small = image.warp(start_image, flows[i], 'bilinear', true)
+--    image_estimate_small = image.warp(start_image, flows)
+
+    if (epoch % 5 == 0 or epoch == 1) then
+      if (i==1) then
+        image.save('new_img'..i..'_'..epoch..'.png', image_estimate_small)
+      end
+    end
+
+    local flow_enlarged = torch.Tensor(2, size1 + 2, size2 + 2):fill(0)
+    flow_enlarged:sub(1,1, 2, size1+1, 2, size2+1):copy(flows[i][1])
+    flow_enlarged:sub(2,2, 2, size1+1, 2, size2+1):copy(flows[i][2])
+
+    for r=2,size1+1 do
+      for s=2,size2+1 do
+        of_r, of_s = torch.round(flow_enlarged[1][r][s] + r), torch.round(flow_enlarged[2][r][s] + s)
+        reg = regularize(flow_enlarged, r, s)
+        if (of_r > 0 and of_r <= size1) and (of_s > 0 and of_s <= size2) then 
+--          ind = min_dir[1][of_r][of_s]
+
+--          derivatives in the direction of U and V
+          dirU = torch.abs(image_estimate[i][1][math.min(of_r+1,size1)][of_s] - image_estimate[i][1][math.max(of_r-1,1)][of_s])
+          dirV = torch.abs(image_estimate[i][1][of_r][math.min(of_s+1,size2)] - image_estimate[i][1][of_r][math.max(of_s-1,1)])
+
+          self.gradInput[i][1][r-1][s-1] = differences[i][1][of_r][of_s] * dirU
+          self.gradInput[i][2][r-1][s-1] = differences[i][1][of_r][of_s] * dirV
+        end
+        self.gradInput[i][1][r-1][s-1] = self.gradInput[i][1][r-1][s-1] + alfa*reg[1]
+        self.gradInput[i][2][r-1][s-1] = self.gradInput[i][2][r-1][s-1] + alfa*reg[2]
+      end
+    end
   end
-  
-  print('grad input size')
-  print(self.gradInput:size())
+
+  gradSums = self.gradInput:sum(1)/batchSize
+  for i=1,batchSize do
+    self.gradInput[i]:copy(gradSums)
+  end
+  epoch = epoch + 5
   return self.gradInput
 end
 
