cba98e75801f6cc7faf2f710a4fa0ff9479f15a9
Alena Moravova <moraval2@lcgpu.felk.cvut.cz>
2016-09-18 17:14:50 +0200

    changed to cuda calculation

--- OpticalFlowCriterion.lua
+++ OpticalFlowCriterion.lua
@@ -1,5 +1,6 @@
 local OpFlowCriterion, parent = torch.class('nn.OpticalFlowCriterion', 'nn.Criterion')
 
+require 'cutorch'
 require 'image'
 require 'sys'
 require 'os'
@@ -27,46 +28,46 @@ function OpFlowCriterion:updateOutput (flows, images)
   size1 = images[1][1]:size(1)
   size2 = images[1][1]:size(2)
 
-  image_estimate = torch.Tensor(batchSize, 3, size1, size2)
+  image_estimate = cutorch.Tensor(batchSize, 3, size1, size2)
 
-  a = torch.Tensor():resizeAs(flows):fill(-1)
-  b = torch.Tensor():resizeAs(flows):fill(-1)
-  total_variation = torch.Tensor():resizeAs(flows)
-  total_variation_der = torch.Tensor():resizeAs(flows)
+  a = cutorch.Tensor():resizeAs(flows):fill(0.00001)
+  b = cutorch.Tensor():resizeAs(flows):fill(0.00001)
+  total_variation = cutorch.Tensor():resizeAs(flows)
+  total_variation_der = cutorch.Tensor():resizeAs(flows)
 
-  local images_l = torch.Tensor(batchSize, size1, size2)
+  -- local images_l = torch.Tensor(batchSize, size1, size2)
 
   for i=1,batchSize do
-    local target = torch.Tensor(channels, size1, size2):copy(images:sub(i,i,channels + 1,channels + channels))
-    image_estimate[i] = image.warp(target, flows[i], 'bilinear')
+    -- local target = torch.Tensor(channels, size1, size2):copy(images:sub(i,i,channels + 1,channels + channels))
+    image_estimate[i] = image.warp(images:sub(i,i,channels + 1,channels + channels), flows[i], 'bilinear'):cuda()
 
-    local flow = torch.Tensor():resizeAs(flows[i]):copy(flows[i])
-    images_l[i]:copy(images:sub(i,i,1, channels):sum(2))
+    -- local flow = torch.Tensor():resizeAs(flows[i]):copy(flows[i])
+    -- images_l[i]:copy(images:sub(i,i,1, channels):sum(2))
 
 --    total variation 
-    a[i]:sub(1,2,1,size1-1,1,size2):copy(flow:sub(1,2,2,size1))
-    b[i]:sub(1,2,1,size1,1,size2-1):copy(flow:sub(1,2,1,size1,2,size2))
+    a[i]:sub(1,2,1,size1-1,1,size2):copy(flows[i]:sub(1,2,2,size1))
+    b[i]:sub(1,2,1,size1,1,size2-1):copy(flows[i]:sub(1,2,1,size1,2,size2))
 
-    a2 = torch.pow(a[i],2)
-    b2 = torch.pow(b[i],2)
+    a2 = cutorch.pow(a[i],2)
+    b2 = cutorch.pow(b[i],2)
 
-    ax = torch.cmul(a[i],flow)
-    bx = torch.cmul(b[i],flow)
+    ax2 = cutorch.cmul(a[i],flows[i]):cmul(2)
+    bx2 = cutorch.cmul(b[i],flows[i]):cmul(2)
 
-    x2 = torch.pow(flow,2)
+    x22 = cutorch.pow(flows[i],2):cmul(2)
 
-    total_variation_der[i] = torch.cdiv(-a[i]-b[i]+2*flow, torch.cmax(torch.pow(a2+b2-2*ax-2*bx+2*x2, 1/2),0.000001))
+    total_variation_der[i] = cutorch.cdiv(a[i]:cmul(-1):csub(b[i]):add(flows[i]):cmul(2), cutorch.cmax(cutorch.pow(a2:add(b2):csub(ax2):csub(bx2):add(x22), 1/2),0.000001))
     
-    total_variation[i][1] = torch.pow(torch.pow(a[i][1] - flow[1],2) + torch.pow(b[i][1] - flow[1],2),1/2)
-    total_variation[i][2] = torch.pow(torch.pow(a[i][2] - flow[2],2) + torch.pow(b[i][2] - flow[2],2),1/2)
+    total_variation[i][1] = cutorch.pow(cutorch.pow(a[i][1]:csub(flows[i][1]),2):add(cutorch.pow(b[i][1]:csub(flows[i][1]),2),1/2))
+    total_variation[i][2] = cutorch.pow(cutorch.pow(a[i][2]:csub(flows[i][2]),2):add(cutorch.pow(b[i][2]:csub(flows[i][2]),2),1/2))
   end
 
-  differences = images_l - image_estimate:sum(2)
+  differences = images:csub(1,batchSize,1, channels):sum(2):csub(image_estimate:sum(2))
 
-  differences = differences/3
+  differences = differences:div(3)
 --  total_variation = total_variation/2
 
-  local diff_sum = torch.abs(differences):sum()
+  local diff_sum = cutorch.abs(differences):sum()
   local tv_sum = total_variation:sum()
 
   self.output = diff_sum + alfa * tv_sum
@@ -81,61 +82,66 @@ end
 -- inputs - optical flow (values for u,v), target = image
 function OpFlowCriterion:updateGradInput (flows, images)
 
-  self.gradInput = torch.Tensor()
+  self.gradInput = cutorch.Tensor()
   self.gradInput:resizeAs(flows):zero()
+  -- self.gradInput:resizeAs(flows)
 
   for i=1,batchSize do
 
-    local flow = torch.Tensor():resizeAs(flows[i]):copy(flows[i])
-    local target = torch.Tensor(channels, size1, size2):copy(images:sub(i,i,channels + 1,channels + channels))
-    local img = torch.Tensor(channels, size1, size2):copy(images:sub(i,i,1,channels)):sum(1)
+    local flow = cutorch.Tensor():resizeAs(flows[i]):copy(flows[i])
+    local target = cutorch.Tensor(channels, size1, size2):copy(images:sub(i,i,channels + 1,channels + channels))
+    local img = cutorch.Tensor(channels, size1, size2):copy(images:sub(i,i,1,channels)):sum(1)
 
 -- gradients in u,v direction
     local flow_shift = torch.Tensor():resizeAs(flow)
 
-    local plus_1_U, minus_1_U, plus_1_V, minus_1_V = torch.Tensor()
+    -- local plus_1_U, minus_1_U, plus_1_V, minus_1_V = torch.Tensor()
 
     flow_shift:copy(flow)
     flow_shift[1] = flow_shift[1] + 0.5
-    plus_1_U = image.warp(target, flow_shift, 'bilinear',0,'pad',-1):sum(1)/3
+    plus_1_U = image.warp(target, flow_shift, 'bilinear',0,'pad',-1):cuda():sum(1):div(3)
 
     flow_shift[1] = flow_shift[1] - 1
-    minus_1_U = image.warp(target, flow_shift, 'bilinear',0,'pad',-1):sum(1)/3
+    minus_1_U = image.warp(target, flow_shift, 'bilinear',0,'pad',-1):cuda():sum(1):div(3)
 
-    local gradU = (minus_1_U - plus_1_U)/1
+    local gradU = cutorch.csub(minus_1_U,plus_1_U)
     gradU[plus_1_U:eq(-1) or minus_1_U:eq(-1)] = 0
 
     flow_shift[1]:copy(flow[1])
     flow_shift[2] = flow_shift[2] + 0.5
-    plus_1_V = image.warp(target, flow_shift, 'bilinear',0,'pad',-1):sum(1)/3
+    plus_1_V = image.warp(target, flow_shift, 'bilinear',0,'pad',-1):cuda():sum(1):div(3)
 
     flow_shift[2] = flow_shift[2] - 1
-    minus_1_V = image.warp(target, flow_shift, 'bilinear',0,'pad',-1):sum(1)/3
+    minus_1_V = image.warp(target, flow_shift, 'bilinear',0,'pad',-1):cuda():sum(1):div(3)
 
-    local gradV = (minus_1_V - plus_1_V)/1
+    local gradV = cutorch.csub(minus_1_V,plus_1_V)
     gradV[plus_1_V:eq(-1) or minus_1_V:eq(-1)] = 0
 
-    self.gradInput[i][1] = torch.cmul(differences[i], gradU)
-    self.gradInput[i][2] = torch.cmul(differences[i], gradV)
+    self.gradInput[i][1] = cutorch.cmul(differences[i], gradU)
+    self.gradInput[i][2] = cutorch.cmul(differences[i], gradV)
 
     -- local TV_minus_U = torch.Tensor():resizeAs(flow[1]):copy(total_variation[i][1])
-    local TV_minus_U = torch.Tensor():resizeAs(flow[1]):fill(-1)
+    local TV_minus_U = cutorch.Tensor():resizeAs(flow[1]):fill(-1)
     TV_minus_U:sub(2,size1):copy(total_variation[i]:sub(1,1,1,size1-1))
 
-    gradU = (TV_minus_U - a[i][1])/2
+    gradU = cutorch.cdiv(cutorch.csub(TV_minus_U, a[i][1]),2)
+    a[i][1]:sub(1,size1,1,1):fill(-1)
+    a[i][1]:sub(1,1,1,size2):fill(-1)
     gradU[TV_minus_U:eq(-1) or a[i][1]:eq(-1)] = 0
 
     -- local TV_minus_V = torch.Tensor():resizeAs(flow[1]):copy(total_variation[i][2])
-    local TV_minus_V = torch.Tensor():resizeAs(flow[1]):fill(-1)
+    local TV_minus_V = cutorch.Tensor():resizeAs(flow[1]):fill(-1)
     TV_minus_V:sub(1,size1,2,size2):copy(total_variation[i]:sub(2,2,1,size1,1,size2-1))
 
-    gradV = (TV_minus_V - b[i][2])/2
+    gradV = cutorch.cdiv(cutorch.csub(TV_minus_V, b[i][2]),2)
+    b[i][2]:sub(1,size1,1,1):fill(-1)
+    b[i][2]:sub(1,1,1,size2):fill(-1)
     gradV[TV_minus_V:eq(-1) or b[i][2]:eq(-1)] = 0
 
     total_variation_der[i][1] = torch.cmul(total_variation_der[i][1], gradU)
     total_variation_der[i][2] = torch.cmul(total_variation_der[i][2], gradV)
 
-    self.gradInput[i] = self.gradInput[i] + alfa*total_variation_der[i]
+    self.gradInput[i] = self.gradInput[i] + total_variation_der[i]:cmul(alfa)
 
 --    clip to [-1, 1]
     self.gradInput[i][self.gradInput[i]:gt(1)] = 1
@@ -144,7 +150,7 @@ function OpFlowCriterion:updateGradInput (flows, images)
   end
 
 --    batch learning
-  gradSums = self.gradInput:sum(1)/batchSize
+  gradSums = self.gradInput:sum(1):div(batchSize)
   for i=1,batchSize do
     self.gradInput[i]:copy(gradSums)
   end
